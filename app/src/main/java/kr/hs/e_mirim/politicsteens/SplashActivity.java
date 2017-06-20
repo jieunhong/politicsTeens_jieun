bf71c7b71d7424c6d82f000dc2cdebf9ed8b79f5
jieun <hyy0786@naver.com>
2017-06-20 18:54:38 +0900

    #1 jieun:Video

---
+++ app/src/main/java/kr/hs/e_mirim/politicsteens/SplashActivity.java
@@ -0,0 +1,27 @@
+package kr.hs.e_mirim.politicsteens;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by User on 2017-06-20.
+ */
+
+public class SplashActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(4000);
+        }catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
